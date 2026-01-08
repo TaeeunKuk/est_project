9e598328d3ba2843bd135a587edaa4efb68d109e
Taeeun Kuk <ktetime2@gmail.com>
2026-01-08 11:33:34 +0900

    update

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,10 +1,17 @@
 // src/models/userModel.js
-const pool = require('../config/db');
+const pool = require("../config/db");
 
 // 이메일로 유저 찾기
 const findByEmail = async (email) => {
-  const query = 'SELECT * FROM users WHERE email = $1';
+  const query = "SELECT * FROM users WHERE email = $1";
   const result = await pool.query(query, [email]);
+  return result.rows[0];
+};
+
+// ID로 유저 찾기
+const findById = async (id) => {
+  const query = "SELECT * FROM users WHERE id = $1";
+  const result = await pool.query(query, [id]);
   return result.rows[0];
 };
 
@@ -20,4 +27,4 @@ const createUser = async (userInfo) => {
   return result.rows[0];
 };
 
-module.exports = { findByEmail, createUser };
+module.exports = { findByEmail, findById, createUser };
